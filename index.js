43aad046aaac9336ebc998b564f7653381c0fe15
aceiny <a_zeraibi@estin.dz>
2023-12-01 03:46:55 +0100

    built schema

---
+++ index.js
@@ -0,0 +1,2 @@
+registration /post 
+
--- schemas/model.js
+++ schemas/model.js
@@ -1,5 +1,30 @@
-const {Schema,model} = require('mongoose')
-const SampleSchema = new Schema({
-    name:String
+const {Schema , model} = require('mongoose');
+
+const userSchema = new Schema({
+    name : {
+        type : String,
+        trim : true,
+    },
+    email : {
+        type : String,
+        unique : true,
+    },
+    team : {
+        type : String,
+    },
+    phoneNumber : {
+        type : String,
+    },
+    institution : {
+        type : String,
+        enum : ['ESI','ESTIN','USTHB','ESI-SBA','Other'],
+    },
+    placeToStay : {
+        type : Boolean
+    },
+    anythingToAdd : {
+        type : String,
+    },
 })
-module.exports = model('SampleSchema'/*collection name in the database*/,SampleSchema/*schema to use on it*/)
+
+module.exports = model('User',userSchema);
